90d51671789139273b335f90b30227ce88aa09ae
Sonu Kumar Singh <36787321+sonukumarsinghanu@users.noreply.github.com>
2024-04-09 12:19:54 +0000

    card making for the result

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
 import "./styles.css";
 import { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 export default function App() {
   const [suggestion, setSuggestion] = useState(false);
-  const [query, setQuery] = useState('')
-  const [data, setData] = useState([])
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
 
   const fetchData = async (query) => {
-
     try {
-      const token = await axios.get(`https://jsonplaceholder.typicode.com/photos/${query}`);
+      const token = await axios.get(
+        `https://jsonplaceholder.typicode.com/photos/${query}`,
+      );
       setData(token.data);
-      console.log(data)
-
-
+    } catch (e) {
+      console.log("getting Error while search via APi", e);
     }
-    catch (e) {
-      console.log('getting Error while search via APi', e)
-    }
-  }
-
+  };
 
   useEffect(() => {
     const timeOut = setTimeout(() => {
-      fetchData(query)
-    }, 100);
+      fetchData(query);
+    }, 1000);
     return () => {
-      clearTimeout(timeOut)
-    }
-
-  }, [query])
+      clearTimeout(timeOut);
+    };
+  }, [query]);
 
   const handleChange = (e) => {
-    setQuery(e.target.value)
-  }
+    setQuery(e.target.value);
+  };
   return (
     <div className="App">
-      <input type='text' name='search' id='search'
+      <input
+        type="text"
+        name="search"
+        id="search"
         placeholder="Enter your Query"
         onFocus={() => setSuggestion(true)}
         onBlur={() => setSuggestion(false)}
-        onChange={handleChange} />
-      {data &&
-        <div className="suggestion" >
-          <div className="card">
-            <img src={data.thumbnailUrl} alt="Avatar" style={{ width: "100%" }} />
-            <div className="container">
-
-              <p>{data.title}</p>
-            </div>
+        onChange={handleChange}
+      />
+      {data && suggestion && (
+        <div className="card">
+          <img src={data.thumbnailUrl} alt="Avatar" style={{ width: "100%" }} />
+          <div className="container">
+            <p>{data.title}</p>
           </div>
         </div>
-      }
+      )}
     </div>
   );
 }
